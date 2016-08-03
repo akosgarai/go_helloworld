8db6355d6fead50b1803d8ea3b577c8d0b35783f
akosgarai <szlem01@gmail.com>
2016-08-03 23:16:36 +0200

    playing with the stringutil functions

--- helloworld.go
+++ helloworld.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "fmt"
+    "github.com/garaiakos/go_stringutil"
 )
 
 func main() {
-    fmt.Printf("Hello World & GO!\n");
+    fmt.Println(go_stringutil.Reverse("Hello World & GO!\n"));
+    a,b := go_stringutil.Swap("Hello", "World!") 
+    fmt.Println(a, b);
 }
